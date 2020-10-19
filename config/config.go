d69f2f94459697d63a613eb11fa78c02ff74c6df
Aleksey Ch <a.a.chubanov@gmail.com>
2020-10-19 17:34:57 +0300

    Little refactoring

--- config/config.go
+++ config/config.go
@@ -7,18 +7,15 @@ import (
 	"os"
 )
 
-// Config of the eapplication
-var Config model.Config
-
 // Init configs
 func Init() model.Config {
-	// Read config
 	file, _ := os.Open("./config.json")
+	// Close file after usage
 	defer file.Close()
-	err := json.NewDecoder(file).Decode(&Config)
-	if err != nil {
+	var c model.Config
+	if err := json.NewDecoder(file).Decode(&c); err != nil {
 		fmt.Println("Cannot read config file:", err)
 		os.Exit(1)
 	}
-	return Config
+	return c
 }
